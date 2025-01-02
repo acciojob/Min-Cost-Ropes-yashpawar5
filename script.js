b5ba45ba68e14b36612b9667fb34bf769c3cccc3
Yash Pawar <89527379+yashpawar5@users.noreply.github.com>
2025-01-02 19:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+    let minHeap = new MinHeap();
+  for (let i = 0; i < arr.length; i++) {
+    minHeap.insert(arr[i]);
+  }
+  let cost = 0;
+  while (minHeap.heap.length > 1) {
+    let first = minHeap.extractMin();
+    let second = minHeap.extractMin();
+    cost += first + second;
+    minHeap.insert(first + second);
+  }
+  return cost;
 }
 
 module.exports=mincost;
